059bc61f8b7e54abf868788b0810497982ed62d3
NahwaKakaa <nahwanirzam@gmail.com>
2024-10-06 13:54:30 +0800

    Menambahkan design layout

--- kasir/src/App.js
+++ kasir/src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Col, Row, Container } from 'react-bootstrap';
 import React from 'react';
-import NavbarComponent from './component/NavbarComponent';
+import {Hasil, ListCategories, NavbarComponent} from "./component";
 
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
